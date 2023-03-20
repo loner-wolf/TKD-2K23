4e3676703ef0742b08fc77b5cf8e67471c027fa0
Aryan Srivastava <74964875+loner-wolf@users.noreply.github.com>
2023-03-20 11:22:42 +0530

    Add files via upload

---
+++ res/js/countdown.js
@@ -0,0 +1,14 @@
+$('.countdown-timer').countdown({
+    date: "May 04, 2023 10:00:00",
+
+    render: function(data) {
+        $(".days .value").html(this.leadingZeros(data.days, 2));
+        $(".hours .value").html(this.leadingZeros(data.hours, 2));
+        $(".minutes .value").html(this.leadingZeros(data.min, 2));
+        $(".seconds .value").html(this.leadingZeros(data.sec, 2));
+    }
+});
+
+$('a').click(function(event){
+    event.preventDefault(); 
+	});
